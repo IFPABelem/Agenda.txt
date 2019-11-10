1f6e62b1c948b22a4bfbc622538cc2da45ba6ae2
TiagoDanin <TiagoDanin@outlook.com>
2019-11-10 13:54:25 -0300

    Improve code style

--- index.py
+++ index.py
@@ -1,85 +1,96 @@
-import ast
-tel = {}
-telAlterado = {}
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import ast # https://docs.python.org/3/library/ast.html
+import sys # https://docs.python.org/3/library/sys.html
+
+# inicio = input('Você deseja adcionar um novo contato?(S/N)')
+
+arquivoNome = "agenda.txt"
+agenda = {}
+taRodando = True
+
+agendaAlterado = {}
 b = 0
 n = 0
 cont=0
 
-def lerTxt():
-    arquivo = open("BD2.txt","r")
-    tel.update(ast.literal_eval(arquivo.read()))
-    arquivo.close()
-    exibirLista()
+def lerAgenda():
+	'''	try:
+			with open('agan.txt', 'r') as f:
+				cont = 1
+				n +=1
+				lerTxt()
+		except IOError:
+			print("Nenhum contato adicionado anteriomente")'''
 
-    
-def adcionar():
-    nome = input('Nome do contato: ').capitalize()
-    numero = input('Número: ')
-    tel[nome] = numero
+	arquivo = open(arquivoNome, "r")
+	agenda.update(ast.literal_eval(arquivo.read()))
+	arquivo.close()
+	return
 
-def exibirLista():
-    conteudoTxt = ""
-    x = n-1
-    telAlterado = sorted(tel.items(), key = lambda t: t[0])
-    telAlterado = dict(telAlterado)
-    for contatoLista,numeroLista in telAlterado.items():
-        conteudoTxt += str(n-x) +')' + contatoLista + ' - ' + numeroLista + '\n'
-        print(n-x,')',contatoLista, ' - ', numeroLista)
-        x-=1
-    arquivo = open("BD.txt", "w")
-    arquivo.writelines(conteudoTxt)
-    arquivo.close()
+def gravarAgenda():
+	return
 
-    arquivo = open("BD2.txt", "w")
-    arquivo.writelines(str(telAlterado))
-    arquivo.close()
-def alterar():
-    exibirLista()
-    contatoAlterado = input('Digite o nome do contato que quer alterar: ').capitalize()
-    #while(contatoAlterado<1 or contatoAlterado>n):
-        #contatoAlterado = int(input('Índice inexistente! Digite um índice válido: '))
-    del tel[contatoAlterado]
-    nome = input('Novo nome: ').capitalize()
-    numero = input('Novo número: ')
-    tel[nome] = numero
+def mostrarAgenda():
+	conteudoTxt = ""
+	x = n-1
+	agendaAlterado = sorted(agenda.items(), key = lambda t: t[0])
+	agendaAlterado = dict(agendaAlterado)
+	for contatoLista,numeroLista in agendaAlterado.items():
+		conteudoTxt += str(n-x) +')' + contatoLista + ' - ' + numeroLista + '\n'
+		print(n-x,')',contatoLista, ' - ', numeroLista)
+		x-=1
+	arquivo = open(arquivoNome, "w")
+	arquivo.wriagendaines(conteudoTxt)
+	arquivo.close()
 
-def excluir():
-    n-1
-    exibirLista()
-    contatoExcluido = input('Digite o nome do contato que quer excluir: ').capitalize()
-    #while(contatoAlterado<1 or contatoAlterado>n):
-        #contatoAlterado = int(input('Índice inexistente! Digite um índice válido: '))
-    del tel[contatoExcluido]
+	arquivo = open(aquivoNome, "w")
+	arquivo.wriagendaines(str(agendaAlterado))
+	arquivo.close()
 
-print('****Agenda telefônica****')
-try:
-   with open('BD2.txt', 'r') as f:
-       cont = 1
-       n +=1
-       lerTxt()
-except IOError:
-    print("Nenhum contato adicionado anteriomente")
+def adcionarContato():
+	nome = input('Nome do contato: ').capitalize()
+	numero = input('Número: ')
+	agenda[nome] = numero
 
-if (cont == 0):
-    inicio = input('Você deseja adcionar um novo contato?(S/N)')
-    if (inicio == 's' or inicio == 'S'):
-        n += 1
-        adcionar()
-        exibirLista()
+def alterarContato():
+	exibirLista()
+	contatoAlterado = input('Digite o nome do contato que quer alterar: ').capitalize()
+	#while(contatoAlterado<1 or contatoAlterado>n):
+		#contatoAlterado = int(input('Índice inexistente! Digite um índice válido: '))
+	del agenda[contatoAlterado]
+	nome = input('Novo nome: ').capitalize()
+	numero = input('Novo número: ')
+	agenda[nome] = numero
 
-while (b!=4 and n>0):
-    b = int(input('Digite o número da opção desejada:\n1 - Adcionar contato\n2 - Alterar contato\n3 - Excluir contato\n4 - Encerrar programa\n'))
-    while(b<1 or b>4):
-        b = int(input('Opção inválida!\nDigite o número da opção desejada:\n1 - Adcionar contato\n2 - Alterar contato\n3 - Excluir contato\n4 - Encerrar programa\n'))
-    if (b == 1):
-        n += 1
-        adcionar()
-        exibirLista()
+def excluirContato():
+	n-1
+	exibirLista()
+	contatoExcluido = input('Digite o nome do contato que quer excluir: ').capitalize()
+	#while(contatoAlterado<1 or contatoAlterado>n):
+		#contatoAlterado = int(input('Índice inexistente! Digite um índice válido: '))
+	del agenda[contatoExcluido]
 
-    if (b == 2):
-        alterar()
-        exibirLista()
+def main():
+	print('****Agenda agendaefônica em TXT****')
+	menuTexto = '''Digite o número da opção desejada:
+	1 - Adcionar contato
+	2 - Alterar contato
+	3 - Excluir contato
+	4 - Encerrar programa
+	'''
 
-    if (b == 3):
-        excluir()
-        exibirLista()
+	while taRodando:
+		opcao = input(menuTexto)
+		if (opcao == "1"):
+			adcionarContato()
+		elif (opcao == "2"):
+			alterarContato()
+		elif (opcao == "3"):
+			excluirContato()
+		elif (opcao == "4"):
+			sys.exit(1)
+		else:
+			print("Opção inválida!\n" + menuTexto)
+
+main()
